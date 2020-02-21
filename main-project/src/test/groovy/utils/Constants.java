410b66004988b3c5916518fbad0d4550509461da
3024k <3024hik@gmail.com>
2020-02-21 13:19:18 +0600

    constant file modified

--- main-project/src/test/groovy/utils/Constants.java
+++ main-project/src/test/groovy/utils/Constants.java
@@ -1,26 +1,26 @@
 package utils;
 
 public class Constants {
-     static public String p2p_ProcessingCode = "400000";
-     static public String cashIn_ProcessingCode = "528500";
-     static public String cashOut_ProcessingCode = "530085";
-     static public String CuBillPay_ProcessingCode = "510068";
-     static public String AgBillPay_ProcessingCode = "518568";
-     static public int p2p_BasicRate = 4;
-     static public String generalCustomer_userSegment = "01";
-     static public String islamicCustomer_userSegment = "03";
-     static public int p2p_inclusiveVat = 15; // in percentage
-     int appPanEntryMode = 81;
-     int ussdPanEntryMode = 77;
-     public static String QADB = "10.88.250.93";
-     public static String QADB_port = "1521";
-     public static String QADB_sid = "konakms";
-     public static String QADB_Password = "Konasl$123";
-     public static String QADB_Username = "KONADFS";
+     static public String p2p_ProcessingCode = "xxxxxx";
+     static public String cashIn_ProcessingCode = "xxxxxx";
+     static public String cashOut_ProcessingCode = "xxxxxx";
+     static public String CuBillPay_ProcessingCode = "xxxxxx";
+     static public String AgBillPay_ProcessingCode = "xxxxxx";
+     static public int p2p_BasicRate = x;
+     static public String generalCustomer_userSegment = "xx";
+     static public String islamicCustomer_userSegment = "xx";
+     static public int p2p_inclusiveVat = xx; // in percentage
+     int appPanEntryMode = xx;
+     int ussdPanEntryMode = xx;
+     public static String QADB = "xxx.xxx.xxx.xxx";
+     public static String QADB_port = "xxxx";
+     public static String QADB_sid = "db_sid";
+     public static String QADB_Password = "db-password";
+     public static String QADB_Username = "db-username";
 
-     public static String QA_HOST_NAME = "root"; // username for remote host
-     public static String HOST_PASSWORD = "Konasl@123"; // password of the remote host
-     public static String HOST_IP = "10.88.250.92"; // remote host address
-     public static int HOST_PORT = 22133;
+     public static String QA_HOST_NAME = "host-name"; // username for remote host
+     public static String HOST_PASSWORD = "password"; // password of the remote host
+     public static String HOST_IP = "xxx.xxx.xxx.xxx"; // remote host address
+     public static int HOST_PORT = xxxxx;
 
 }
